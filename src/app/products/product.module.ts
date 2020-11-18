ee319b332120542e8d85d6b50d49b0c96df29ac1
christy <christyshafack@gmail.com>
2020-11-18 22:05:35 +0100

    Adding child routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { ProductListComponent } from './product-list.component';
 import { ProductResolver } from './product-resolver.service'
 import { RouterModule } from '@angular/router'
@@ -16,13 +18,30 @@ import { SharedModule } from '../shared/shared.module';
       resolve: { resolvedData: ProductResolver} },
       { path: 'products/:id/edit', 
       component: ProductEditComponent,
-      resolve: { resolvedData: ProductResolver} }
+      resolve: { resolvedData: ProductResolver},
+      children:[
+        {
+          path: '',
+          redirectTo: 'info',
+          pathMatch: 'full'
+        },
+        {
+          path: 'info',
+          component: ProductEditInfoComponent
+        },
+        {
+          path: 'tags',
+          component: ProductEditTagsComponent
+        }
+      ]}
     ]),
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
