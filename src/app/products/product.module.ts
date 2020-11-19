d89177c008400d7003c9a6bafeaf92f348fc4648
christy <christyshafack@gmail.com>
2020-11-19 10:14:54 +0100

    Grouping routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -12,28 +12,20 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', 
-      component: ProductDetailComponent, 
-      resolve: { resolvedData: ProductResolver} },
-      { path: 'products/:id/edit', 
-      component: ProductEditComponent,
-      resolve: { resolvedData: ProductResolver},
+      { path: 'products', component: ProductListComponent,
       children:[
-        {
-          path: '',
-          redirectTo: 'info',
-          pathMatch: 'full'
-        },
-        {
-          path: 'info',
-          component: ProductEditInfoComponent
-        },
-        {
-          path: 'tags',
-          component: ProductEditTagsComponent
-        }
-      ]}
+        { path: ':id', 
+        component: ProductDetailComponent, 
+        resolve: { resolvedData: ProductResolver} },
+        { path: ':id/edit', 
+        component: ProductEditComponent,
+        resolve: { resolvedData: ProductResolver},
+        children:[
+          { path: '', redirectTo: 'info', pathMatch: 'full'},
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent }
+        ]}
+      ]},
     ]),
   ],
   declarations: [
